d917f7a06e3bc1c03889944471cab3dc7ec68ccf
muhmmadjon-usmonov <muhammadjonusmonov001@gmail.com>
2022-01-06 18:42:34 +0500

    js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+alert ("Assalomu alaykum va rohmatullohi va barokatuh!")
+var userName = prompt("Ismingizni ayting!")
+console.log("Ismingiz:"+ userName);
+var surName = prompt("Familyangiz nima?")
+console.log("Familiyangiz:"+ surName);
+var age = prompt("Yoshingiz?")
+console.log("Yoshingiz:"+ age);
+prompt("Qayerdansiz?")
+var nationality = prompt("Millatingiz nima?")
+console.log=("Millatingiz:"+ nationality)
+var study = prompt("Qayerda o'qisiz?")
+console.log=("Unversitetiz:"+ study);
+var kasb=prompt("Nima sohada o'qisiz?")
+console.log=("Sohangiz:"+ kasb);
+alert ("Javobingiz uchun rahmat!!!")
